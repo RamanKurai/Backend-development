ce845fb2a028a3d54912f2880db449751c8eba14
Raman Kurai <ramankurai27@gmail.com>
2025-02-05 22:27:02 +0530

    again practice of creating http server

--- express/index.js
+++ express/index.js
@@ -1,16 +1,12 @@
 const express = require("express")
-
 const app = express();
-
 const users = [{
     users : "John",
     kidneys:[{
         healthy : false
+}], healthy : true
 }]
-}]
-
 app.use(express.json())
-
 app.get("/" ,(req , res)=>{
     const johnKidneys = users[0].kidneys;
     const numberofKidneys = johnKidneys.length;
@@ -28,7 +24,6 @@ app.get("/" ,(req , res)=>{
         numberofUnhealthyKidneys
     })
 })
-
 app.post("/" , (req , res)=>{
     const isHealthy = req.body.isHealthy;
     users[0].kidneys.push({
