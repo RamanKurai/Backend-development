7971cadec41ab7b9b858c0c0c09b59643e02b43f
Raman Kurai <ramankurai27@gmail.com>
2025-02-06 21:10:48 +0530

    learning middleware part 2

--- middleware/indexmiddleware.js
+++ middleware/indexmiddleware.js
@@ -1,41 +1,29 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
 // funtion that have boolean for age greater than 14 or not
-// we are assuming this as the middleware to get the desired endpoint to have rides
-function isOldEnough(age) {
-    if (age >= 14) {
-       return true
-    } else {
-        return false
-    }
+// in this part I made a official middleware , how are the middleware were already introduce!
+function isOldEnoughMiddleware(req, res, next) {
+  const age = req.query.age;
+  if (age >= 14) {
+    next();
+    return true;
+  } else {
+    return false;
+  }
 }
 
-app.get("/ride" , (req , res)=>{
-  if (isOldEnough(req.query.age)) {
-    res.json({
-        msg: "You can sucessfullyy riden the ride1"
-    })
-  } else {
-    res.status(411).json({
-        msg : "Sorry you are not the age of 14"
-    })
-  }
-    
-})
+app.get("/ride", isOldEnoughMiddleware, (req, res) => {
+  res.json({
+    msg: "You can sucessfullyy riden the ride1",
+  });
+});
 
-app.get("/ride1" , (req , res)=>{
-    if (isOldEnough(req.query.age)) {
-      res.json({
-          msg: "You can sucessfullyy riden the ride1"
-      })
-    } else {
-      res.status(411).json({
-          msg : "Sorry you are not the age of 14"
-      })
-    }
-      
-  })
+app.get("/ride1", isOldEnoughMiddleware, (req, res) => {
+  res.json({
+    msg: "You can sucessfullyy riden the ride1",
+  });
+});
 
-app.listen(3000)
+app.listen(3000);
