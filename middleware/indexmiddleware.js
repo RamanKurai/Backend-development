12d4e79f063bd08156b268e7cd1e4c9935c98cf1
Raman Kurai <ramankurai27@gmail.com>
2025-02-06 22:10:46 +0530

    middleware : error expection

--- middleware/indexmiddleware.js
+++ middleware/indexmiddleware.js
@@ -1,37 +1,20 @@
 const express = require('express');
+
 const app = express();
+let errorCount = 0;
+
 // You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// rate limit the requests from a user to only 5 request per second
-// If a user sends more than 5 requests in a single second, the server
-// should block them with a 404.
-// User will be sending in their user id in the header as 'user-id'
-// You have been given a numberOfRequestsForUser object to start off with which
-// clears every one second
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 3000)
-
-app.use(function (req , res , next) {
-    const userId = req.headers["user-id"]
-    if (numberOfRequestsForUser[userId]) {
-        numberOfRequestsForUser = numberOfRequestsForUser + 1;
-        if (numberOfRequestsForUser[userId] > 5) {
-            res.status(404).send("No entry")
-        } else{
-            next();
-        }
-    }
-    else{
-        numberOfRequestsForUser[userId] = 1
-        next();
-    }
+app.use(function (err , req , res , next) {
+    res.status(404).send({})
+    errorCount = errorCount + 1;
 })
 
-
 app.get('/user', function(req, res) {
+  throw new Error("User not found");
   res.status(200).json({ name: 'john' });
 });
 
@@ -39,6 +22,14 @@ app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
+app.get('/errorCount', function(req, res) {
+    
+  res.status(200).json({ errorCount });
+});
+
+
+
 
 app.listen(3000)
+
 module.exports = app;
