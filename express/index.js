024f273d422e61e108a06e68d09ef71eeb83233e
Raman Kurai <ramankurai27@gmail.com>
2025-02-09 15:50:34 +0530

    Dynamic routes in query paramters

--- express/index.js
+++ express/index.js
@@ -5,33 +5,33 @@ const app = express();
 //  that is another method to take input from the user
 app.use(express.json())
 
-app.get("/sum" , (req , res)=>{
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+app.get("/sum/:a/:b" , (req , res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
     res.json({
         answer : a + b
     })
 })
 
-app.get("/multiply" , (req , res)=>{
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+app.get("/multiply/:a/:b" , (req , res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
     res.json({
         answer : a * b
     })
 })
 
-app.get("/divide" , (req , res)=>{
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+app.get("/divide/:a/:b" , (req , res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
     res.json({
         answer : a / b
     })
 })
 
-app.get("/subtract" , (req , res)=>{
-    const a = parseInt(req.query.a)
-    const b = parseInt(req.query.b)
+app.get("/subtract/:a/:b" , (req , res)=>{
+    const a = parseInt(req.params.a)
+    const b = parseInt(req.params.b)
     res.json({
         answer : a - b
     })
